963384e932b0c75db914035db2ea327aa451f911
imtarunrai <tarunrai484@gmail.com>
2026-02-06 18:27:56 +0530

    allow all origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,14 +13,12 @@ import errorHandler from "./middlewares/errorHandler.js";
 
 const app = express();
 
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//   }),
-// );
-
-app.use(cors()); 
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  }),
+);
 
 app.use(express.json());
 
